5a5093a0b71875a5456e614bb54c3bc0fa7d6c93
sadahussain123567 <159890907+sadahussain123567@users.noreply.github.com>
2024-03-11 05:13:09 -0600

    Initial commit

---
+++ add.js
@@ -0,0 +1,16 @@
+var age = 100;
+if (age < 10) {
+    console.log("this is a child.");
+}
+else if (age < 20) {
+    console.log("this is a teenager.");
+}
+else if (age < 30) {
+    console.log("this is a young.");
+}
+else if (age < 65) {
+    console.log("this is a elder.");
+}
+else {
+    console.log("this is not find");
+}
